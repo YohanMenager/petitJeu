f4d7af308ddf5205d567d19e05a41c34dcfc744a
YohanMenager <yohan.menager0600@gmail.com>
2025-02-09 21:26:08 +0100

    tentative de résolution d'un problème avec le son

--- js/Sons.js
+++ js/Sons.js
@@ -1,11 +1,11 @@
 export default class Sons {
-    static win = new Audio('../assets/sons/success.mp3');
-    static lose = new Audio('../assets/sons/fail.mp3');
-    static start = new Audio('../assets/sons/start.mp3');
-    static bonus = new Audio('../assets/sons/bonus.mp3');
-    static powerup = new Audio('../assets/sons/powerup.mp3');
-    static music = new Audio('../assets/sons/music.mp3');
-    static jump = new Audio('../assets/sons/jump.mp3');
+    static win = new Audio('./assets/sons/success.mp3');
+    static lose = new Audio('./assets/sons/fail.mp3');
+    static start = new Audio('./assets/sons/start.mp3');
+    static bonus = new Audio('./assets/sons/bonus.mp3');
+    static powerup = new Audio('./assets/sons/powerup.mp3');
+    static music = new Audio('./assets/sons/music.mp3');
+    static jump = new Audio('./assets/sons/jump.mp3');
 
     static playJump() {
         this.jump.play();
